ace9e07750cd0adc26087687015bd8a409d7b196
Pedro <ThanksUniverse333@outlook.com>
2023-01-12 01:48:56 -0300

    server update

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post("/", async (req, res) => {
 	}
 });
 
-app.listen(5000, () => console.log(' Server is running on port http://localhost:5000 '));
+app.listen(5000, () => console.log(' Server is running on port https://pedro-ai.onrender.com'));
