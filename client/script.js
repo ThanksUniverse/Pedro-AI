bc7d46d1cad1df171f07e1c2013adfe4bd9213b6
Pedro <ThanksUniverse333@outlook.com>
2023-01-12 07:55:57 -0300

    update client

--- client/script.js
+++ client/script.js
@@ -6,40 +6,49 @@ const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
-
 let storedName = localStorage.getItem("name");
-let yourName
-console.log(storedName)
-if (storedName !== 'null' && storedName !== null) {
-  yourName = localStorage.getItem("name")
+let yourName;
+if (storedName !== "null" && storedName !== null) {
+	yourName = localStorage.getItem("name");
 } else {
-  yourName = window.prompt("Whats your name ?")
-  localStorage.setItem("name", yourName);
-  console.log(yourName)
+	yourName = window.prompt("Whats your name ?");
+	localStorage.setItem("name", yourName);
 }
+
+let storedProfile = localStorage.getItem("profile");
+let yourProfile;
+console.log(storedProfile);
+if (storedProfile !== "null" && storedProfile !== null) {
+	yourProfile = localStorage.getItem("profile");
+} else {
+	yourProfile = "/assets/user.svg";
+}
+
+let userName = document.getElementById('yourUsername')
+userName.innerHTML = yourName;
 
 // Load the bot message
 function loader(element) {
-  element.textContent = ''
-  let loadingLetters = ["L", "o", "a", "d", "i", "n", "g", ".", ".", "."]
+	element.textContent = "";
+	let loadingLetters = ["L", "o", "a", "d", "i", "n", "g", ".", ".", "."];
 
-  let numbers = 0
-  loadInterval = setInterval(() => {
-    numbers += 1
-      // Update the text content of the loading indicator
-      element.textContent += loadingLetters[numbers-1];
+	let numbers = 0;
+	loadInterval = setInterval(() => {
+		numbers += 1;
+		// Update the text content of the loading indicator
+		element.textContent += loadingLetters[numbers - 1];
 
-      // If the loading indicator has reached three dots, reset it
-      if (element.textContent === 'Loading...') {
-        element.textContent = '';
-        numbers = 0
-      }
-  }, 300);
+		// If the loading indicator has reached three dots, reset it
+		if (element.textContent === "Loading...") {
+			element.textContent = "";
+			numbers = 0;
+		}
+	}, 150);
 }
 
 // Bot typing Text...
 function typeText(element, text) {
-  element.innerHTML = text
+	element.innerHTML = text;
 }
 
 // Generate unique id for bot message
@@ -50,19 +59,23 @@ function generateUniqueId() {
 
 	return `id-${timeStamp}-${hexadecimalString}`;
 }
-
+let repeatMessage;
 // Lines in chat based if is or not the bot message
 function chatStripe(isAi, value, uniqueId) {
-  console.log(yourName);
-  if (yourName == undefined) {
-    yourName = "User"
-  }
+	if (yourName == undefined) {
+		yourName = "User";
+	}
+	repeatMessage = "";
+	if (user) {
+		repeatMessage = value;
+	}
+	console.log(yourProfile);
 	return `
       <div class="wrapper ${isAi && "ai"}">
           <div class="chat">
               <div class="profile">
                   <img 
-              src="${isAi ? bot : user}"
+              src="${isAi ? bot : yourProfile}"
               alt="${isAi ? "bot" : "user"}"
               />
               </div>
@@ -77,15 +90,17 @@ let waitingResults = 0;
 // Handle the message sent
 const handleSubmit = async (e) => {
 	e.preventDefault();
-  if (waitingResults === 1) {
-    return
-  }
-  waitingResults = 1;
+	if (waitingResults === 1) {
+		return;
+	}
+	waitingResults = 1;
 
 	const data = new FormData(form);
 
 	// user's chatstripe
 	chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+	console.log(repeatMessage);
 
 	form.reset();
 
@@ -118,24 +133,46 @@ const handleSubmit = async (e) => {
 		const data = await response.json();
 		const parsedData = data.bot.trim();
 
-    console.log({parsedData})
+		console.log({ parsedData });
 
 		typeText(messageDiv, parsedData);
 
-    waitingResults = 0;
+		waitingResults = 0;
 	} else {
 		const err = await response.text();
 
 		messageDiv.innerHTML = "Something went wrong";
-		alert(err);
+		//alert(err);
+		document.getElementById("placeIWrite").innerHTML = repeatMessage;
 
-    waitingResults = 0;
+		waitingResults = 0;
 	}
 };
 
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("keyup", (e) => {
-	if (e.keyCode === 13 && !(e.shiftKey)) {
+	if (e.keyCode === 13 && !e.shiftKey) {
 		handleSubmit(e);
+	}
+});
+
+const profilePicture = document.getElementById("profile_picture");
+if (yourProfile) {
+	profilePicture.src = yourProfile;
+}
+
+// Add a check to make sure the URL is valid
+profilePicture.addEventListener("click", function () {
+	// Prompt the user for a new background image URL
+	var newBackgroundImageURL = prompt("Please enter a new background image URL:");
+
+	// Check if the URL is valid
+	if (newBackgroundImageURL && newBackgroundImageURL.match(/^https?:\/\/.*\.(jpg|png|gif)$/)) {
+		// Set the background image of the body element to the new URL
+		profilePicture.src = newBackgroundImageURL;
+		localStorage.setItem("profile", profilePicture.src);
+    yourProfile = localStorage.getItem("profile");
+	} else {
+		alert("Please enter a valid image URL"); // Alert user if invalid URL is entered
 	}
 });
